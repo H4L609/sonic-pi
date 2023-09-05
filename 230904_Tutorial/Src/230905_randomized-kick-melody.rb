4ee72cf7682ea6c008a0373f65abb9c5058db590
Hiroaki Kurokawa <hiroaki.kurokawa@comp-inc.com>
2023-09-05 18:13:10 +0900

    メロ

--- 230904_Tutorial/Src/230905_randomized-kick-melody.rb
+++ 230904_Tutorial/Src/230905_randomized-kick-melody.rb
@@ -10,17 +10,23 @@ live_loop :live do
   syn_res    = rrand(0.6,0.9)
   
   scale_offset = rrand_i(-12, 12)
+  kick_rate    = rrand(1.0,1.5)
   
   sample :bd_mehackit, rate: 1.5
+  
+  sample :bd_boom, rate: kick_rate, amp: 0.5
+  sleep 1
+  sample :bd_boom, rate: kick_rate-0.3, amp: 0.5
+  sleep 1
   
   if one_in(2)
     rhythm = 2.0
     
-  elsif one_in(2)
+  elsif one_in(6)
     rhythm = 4.0
-  elsif one_in(4)
+  elsif one_in(6)
     rhythm = 5.0
-  elsif one_in(8)
+  elsif one_in(6)
     rhythm = 6.0
   else
     rhythm = 7.0
@@ -32,13 +38,20 @@ live_loop :live do
     else
       sample :hat_zan, rate: rrand(0.9, 1.3), cutoff: hat_2_cutoff, res: rrand(0.7,0.9)
     end
+    if rhythm != 2.0
+      sleep 2.0 / rhythm
+    end
   end
   if rhythm == 2.0
     with_fx :bitcrusher, mix: 0.9, amp: 0.7, bits: 6, sample_rate: 20000 do
-      play_pattern_timed scale(:G4+scale_offset, :mixolydian).shuffle.take(4), 0.33, amp: 0.4, cutoff: syn_cutoff, res: syn_res
+      ##| play_pattern_timed scale(:G4+scale_offset, :mixolydian).shuffle.take(4), 0.33, amp: 0.4, cutoff: syn_cutoff, res: syn_res
     end
     sleep 1.8
-  else
-    sleep 2.0 / rhythm
   end
+end
+
+live_loop :riff do
+  use_synth :hoover
+  play scale(:F5, :minor_pentatonic).shuffle.take(2), amp: 0.3, release: 0.2
+  sleep 0.5 + rrand_i(-3,3)*0.125
 end
